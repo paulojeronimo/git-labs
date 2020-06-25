40458fec2503e1f4b82d1d74c12ce0005d1c503b
Paulo Jerônimo <paulojeronimo@gmail.com>
2020-06-25 14:06:56 -0300

    Improve README and docs

--- README.update.sh
+++ README.update.sh
@@ -7,10 +7,11 @@ cat > README.adoc <<EOF
 :icons: font
 :toc:
 :toc-placement!:
+:nofooter:
 ifdef::env-github,env-browser[:outfilesuffix: .adoc]
 
 ifdef::env-github[]
-Please read this link:README.adoc[] through the https://gitlab.com/paulojeronimo/git-labs[GitLab mirror of this project^]. +
+Please read the entire content of this link:README.adoc[] (and other link:docs[]) through the https://gitlab.com/paulojeronimo/git-labs[GitLab mirror of this project^]. +
 This is because https://github.com/github/markup/issues/1095[GitHub does not render \`includes\` in Asciidoc documents correctly^]. 😒
 endif::[]
 
@@ -19,14 +20,9 @@ ifndef::env-github[]
 include::asciidoc-una/uris.adoc[]
 include::asciidoc-una/attributes.adoc[]
 
-// Local attributes
-:Base58: <<base58,Base58>>
-
 Some labs to test {Git} features or solutions.
 
-toc::[]
-
-<<contribution-guidelines,Contributions>> are welcome! 😃 +
+<<docs/contribution-guidelines#,Contributions>> are welcome! 😃 +
 Fell free fork {uri-paulojeronimo-git-labs}[this repo^], add your lab, and {uri-github-creating-a-pull-request-from-a-fork}[make a pull request^] to add it.
 Currently, these scripts only use {Bash}, and tools available in macOS and Linux distros like {sed} and {awk}.
 
@@ -41,23 +37,16 @@ Why we have this mirror in GitLab?
 Well, GitHub does not render Asciidoc documents correctly as Gitlab does.
 You can note, for example, that the clause \`includes\` in Asciidoc does not work in GitHub.
 endif::[]
+endif::[]
 
-== Labs
+Labs:
 
+[%reversed]
 $(for f in `ls lab???.sh | sort -r`
   do
     d=$(git log --diff-filter=A --follow --format=%aI -1 -- $f)
-    sed -n '2p' $f | sed "s/^# Purpose: \(.*\)/* link:$f[] (created in $d): \1/"
+    sed -n '2p' $f | sed "s/^# Purpose: \(.*\)/. link:$f[] (created in $d): \1/"
   done)
-
-:leveloffset: +1
-
-include::docs/contribution-guidelines.adoc[]
-
-include::docs/utilities.adoc[]
-
-:leveloffset: -1
-endif::[]
 EOF
 
 adoc_args="-o index.html"
@@ -74,7 +63,7 @@ do
       ;;
     --html)
       asciidoctor README.adoc $adoc_args
-      for f in docs/lab???.adoc; do asciidoctor $f; done
+      for f in docs/*.adoc; do asciidoctor $f; done
       ;;
     --serve)
       ruby -run -e httpd . -p 8000 &> httpd.log &
